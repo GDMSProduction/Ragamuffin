0e55520b2c173b3c07000ce757fbe01994287d52
Arthur Parker <aeparker101@mail.com>
2018-02-08 20:38:01 -0500

    spder

---
+++ Ragamuffin/Assets/spider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spider : MonoBehaviour {
+    [SerializeField]
+    Rigidbody2D rb2d;
+    [SerializeField]
+    bool realup;
+    [SerializeField]
+    float speed = 0.2f;
+    [SerializeField]
+    float heght;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (realup == true)
+        {
+            transform.Translate(Vector2.up*speed);
+        }
+        if (transform.position.y > heght)
+        {
+            // load scene;
+            realup = false;
+        }
+
+    }
+}
