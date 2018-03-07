ba977bc0df281e635c51b0ddcabb4d66f918011b
Arthur Parker <aeparker101@mail.com>
2018-03-06 19:36:42 -0500

    checkpoint.cs happy face

--- Ragamuffin/Assets/Scripts/death.cs
+++ Ragamuffin/Assets/Scripts/death.cs
@@ -8,6 +8,10 @@ public class death : MonoBehaviour {
     [SerializeField]
     PlayerHeath heath;
   public  bool delaydeath;
+    [SerializeField]
+    float lives = 2;
+  
+  public  GameObject mainSpanpoint;
 	// Use this for initialization
 	void Start () {
 		
@@ -15,23 +19,42 @@ public class death : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(transform.position.y < -20||heath.GetHeath() <=0&&delaydeath==false)
+        if(transform.position.y < -20||heath.GetHeath() <=0&&delaydeath==false&&lives!=0)
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            if(respawn!=null)
             transform.position = respawn.transform.position;
+            else
+            {
+                transform.position = mainSpanpoint.transform.position;
+            }
             heath.ResetHeath();
+            lives -= 1;
         }
         else if(delaydeath)
         {
             StartCoroutine(catdeath());
+        }
+        else if (lives == 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            lives = 3;
+
+            transform.position = mainSpanpoint.transform.position;
         }
 
     }
     IEnumerator catdeath()
     {
         yield return new WaitForSeconds(2);
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
         transform.position = respawn.transform.position;
         heath.ResetHeath();
+        lives -= 1;
         StopAllCoroutines();
+    }
+    public void setRespawn(GameObject _respawn)
+    {
+        respawn = _respawn;
     }
 }
---
+++ Ragamuffin/Assets/checkpoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkpoint : MonoBehaviour {
+
+    // Use this for initialization
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        other.GetComponent<death>().setRespawn(gameObject);
+    }
+}
