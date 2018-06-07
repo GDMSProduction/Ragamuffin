05fc03cc8f2dd334cc01f5764cc5c8605d1a0644
Arthur Parker <aeparker101@mail.com>
2018-06-07 15:34:02 -0400

    Push

---
+++ Ragamuffin/Assets/SwitchingZ.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchingZ : MonoBehaviour {
+    static bool forwardZObjectsActive=true;
+    [SerializeField]
+    List<GameObject> FowardObjects = new List<GameObject>();
+    [SerializeField]
+    List<GameObject> BehindObjects = new List<GameObject>();
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame  
+	void Update () {
+        if (forwardZObjectsActive == true)
+        {
+            for (int i = 0; i < FowardObjects.Count; ++i)
+            {
+                FowardObjects[i].GetComponent<BoxCollider2D>().enabled = true;
+            }
+            for (int i = 0; i < BehindObjects.Count; ++i)
+            {
+                BehindObjects[i].GetComponent<BoxCollider2D>().enabled = false;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < FowardObjects.Count; ++i)
+            {
+                FowardObjects[i].GetComponent<BoxCollider2D>().enabled = false;
+            }
+            for (int i = 0; i < BehindObjects.Count; ++i)
+            {
+                BehindObjects[i].GetComponent<BoxCollider2D>().enabled = true;
+            }
+        }
+	}
+}
