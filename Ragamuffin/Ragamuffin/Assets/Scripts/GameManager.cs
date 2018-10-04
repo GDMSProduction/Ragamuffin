e41b47603ee203faaf14cf725cedf2695f517872
Dylan Rader <dylandarthrader@gmail.com>
2018-10-04 12:39:28 -0400

    Changes: -Blocked out half of hallway level -Added GameManager Prefab & Script

---
+++ Ragamuffin/Ragamuffin/Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	////######## VARIABLES ########////
+
+	public static GameManager gameManager = null;
+	public Scene sceneCurrent;
+
+	////######## PREBUILT METHODS ########////
+
+	void Awake()
+	{
+		if (!gameManager)
+		{
+			gameManager = this;
+		}
+		if (gameManager != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+
+	////######## CUSTOM METHODS ########////
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	// SCENE MANAGMENT //
+	public void LoadNextScene()
+	{
+		SceneManager.LoadScene(gameManager.sceneCurrent.buildIndex + 1);
+	}
+
+	public void LoadPreviousLevel() // FOR DEBUG ONLY
+	{
+		SceneManager.LoadScene(gameManager.sceneCurrent.buildIndex - 1);
+	}
+
+	public void LoadSceneByName(string _levelName)
+	{
+		SceneManager.LoadScene(_levelName);
+	}
+
+	public void LoadSceneByIndex(int _levelNumber)
+	{
+		SceneManager.LoadScene(_levelNumber);
+	}
+
+}
