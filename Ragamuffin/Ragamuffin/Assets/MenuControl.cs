7f85ff6777b4dca05a6c599414f735e3a1a75857
Simon Gosselin <sdgosselin@student.fullsail.edu>
2018-11-09 15:49:18 -0500

    Added a rudimentary Menu system
    New prefab object "MenuControl" must be added to every scene to add menu functunality. Also, the build order must be changed so that MainMenu comes first, then OptionsMenu, then the rest of all scenes.

---
+++ Ragamuffin/Ragamuffin/Assets/MenuControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class MenuControl : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetButtonDown("Cancel"))
+        {
+            PlayerPrefs.SetString("lastScene", SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene("OptionsMenu");
+        }
+	}
+}
