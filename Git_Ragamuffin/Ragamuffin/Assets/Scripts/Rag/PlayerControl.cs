7f02be78e05ea014ebb80dc08c41930d47ba28b7
MarvinDBlue <marvindblue@gmail.com>
2019-01-13 16:36:06 -0500

    Modified and fixed character collision, finalized jump, and implemented/tested controller input
    Character's collision is now lined up with model after previous model adjustments.
    Character collision issues are fixed, no more sticking to objects when moving forward/falling.
    Jump is now implemented and cannot be spammed.
    Controller support is now implemented for movement(left thumb stick) and jumping(A button).

--- Git_Ragamuffin/Ragamuffin/Assets/Scripts/Rag/PlayerControl.cs
+++ Git_Ragamuffin/Ragamuffin/Assets/Scripts/Rag/PlayerControl.cs
@@ -6,37 +6,54 @@ public class PlayerControl : MonoBehaviour
 {
     Rigidbody2D rb2D;
     [SerializeField]
-    float moveSpeed = 5.0f;
+    float moveSpeed = 6.0f;
     [SerializeField]
-    float jumpForce = 10.0f;
-    float input;
-    [SerializeField]
-    bool jump;
+    float jumpForce = 3.0f;
     float jumpTimer;
+    float input;
     bool grounded;
+    //allowJump is for resetting the ability to jump on button press
+    //this keeps the player from holding down the button and making the player character contantly jump
+    bool allowJump;
+    //jumpRequest is for when the player presses the jump button
+    bool jumpRequest;
     bool disableInput;
+
     // Use this for initialization
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
-        jump = false;
         grounded = true;
         disableInput = false;
+        jumpRequest = false;
+        allowJump = true;
+        jumpTimer = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        input = Input.GetAxis("Horizontal");
-        
-        if (!jump)
+        //obtain input from player
+        if (!disableInput)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                jump = true;
-            }
+            input = Input.GetAxis("Horizontal");
         }
+
+        //check for jump
+        if (Input.GetAxis("Jump") > 0)
+        {
+            jumpRequest = true;
+        }
+        //this resets the ability to jump when the player releases the jump button
+        else
+        {
+            allowJump = true;
+        }
+        
+        
+        //this rotates the player left/right based on left/right movement keys
+        //left input is negative, or less than zero
+        //right input is positive
         if (input < 0)
         {
             transform.rotation = Quaternion.Euler(transform.rotation.x, 0, transform.rotation.z);
@@ -45,31 +62,42 @@ public class PlayerControl : MonoBehaviour
         {
             transform.rotation = Quaternion.Euler(transform.rotation.x, 180, transform.rotation.z);
         }
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 0.05f);
-
-        //Debug.Log(hit.collider.name);
-
-        if (hit && hit.collider != this)
-        {
-            grounded = true;
-        }
-        else
-        {
-            grounded = false;
-        }
     }
 
     //using Fixed Update for the manipulation of a Rigidbody
     void FixedUpdate()
     {
+        Movement();
+        Jump();
+    }
+
+    //for movement
+    //simple but effective, and speed can be motified through the inspector
+    void Movement()
+    {
         Vector2 moveVelocity = new Vector2(input * (moveSpeed * 10.0f) * Time.deltaTime, rb2D.velocity.y);
         rb2D.velocity = moveVelocity;
-        if (jump)
+    }
+
+    //jump action and parameter cleanup
+    void Jump()
+    {
+        if (grounded && jumpRequest && allowJump)
         {
             rb2D.velocity += new Vector2(0, jumpForce);
-            jump = false;
+            grounded = false;
+            allowJump = false;
+        }
+        jumpRequest = false;
+    }
 
+    //checks for player collision with ground
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //layer 9 is the Ground layer
+        if (collision.gameObject.layer == 9)
+        {
+            grounded = true;
         }
     }
 }
