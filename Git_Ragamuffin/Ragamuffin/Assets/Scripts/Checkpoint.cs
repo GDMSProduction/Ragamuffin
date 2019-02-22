2e0773e50f7c0de3aba04c88fde4aaafc13796fb
Tim Seaorc <timeseaorc@gmail.com>
2019-02-22 09:52:19 -0500

    Created new checkpoint system

---
+++ Git_Ragamuffin/Ragamuffin/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public static Transform CurrentCheckpoint;
+    [SerializeField] bool Reusable;
+    [SerializeField] Transform SpawnPoint;
+    [SerializeField] GameObject Effect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (SpawnPoint != null)
+                CurrentCheckpoint = SpawnPoint;
+            else
+                CurrentCheckpoint = transform;
+
+            if (Effect != null && CurrentCheckpoint != SpawnPoint)
+                Destroy(Instantiate(Effect, transform.position, transform.rotation), .5f);
+
+            if (!Reusable)
+                gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (SpawnPoint != null)
+                CurrentCheckpoint = SpawnPoint;
+            else
+                CurrentCheckpoint = transform;
+
+            if (Effect != null && CurrentCheckpoint != SpawnPoint)
+                Destroy(Instantiate(Effect, transform.position, transform.rotation), .5f);
+
+            if (!Reusable)
+                gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Git_Ragamuffin/Ragamuffin/Assets/Scripts/ReSpawnManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReSpawnManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+            ReSpawn();
+    }
+
+    public void ReSpawn()
+    {
+        transform.position = Checkpoint.CurrentCheckpoint.position;
+        transform.rotation = Checkpoint.CurrentCheckpoint.rotation;
+    }
+}
