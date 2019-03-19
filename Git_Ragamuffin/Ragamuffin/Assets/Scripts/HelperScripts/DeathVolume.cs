4704d3a8d7d678056cee26252a8eecfd38a676c2
Cainden <Cainden1996@gmail.com>
2019-03-18 23:20:08 -0400

    adding the script for the death volume prefab

---
+++ Git_Ragamuffin/Ragamuffin/Assets/Scripts/HelperScripts/DeathVolume.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------\\
+//             Author: Robert Bauerle
+//               Date: 2/28/2019
+//            Purpose: A simple script to call the respawn function for when the player falls off of the map
+// Associated Scripts: ReSpawnManager
+//--------------------------------------------------------------------------------------------------------------------------------------------------\\
+
+[RequireComponent(typeof(Collider2D))]
+public class DeathVolume : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<ReSpawnManager>().ReSpawn();
+        }
+    }
+}
