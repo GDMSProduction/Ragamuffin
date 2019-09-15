cd773433b776d771fe0fadd2b6baf6de0ffa327b
Colby Peck <CLPeck1@student.fullsail.edu>
2019-09-15 06:08:14 -0400

    Cat's state machine refactor pt. 1
    CatManager.cs:
       -Added TODO list
       -Added StateMachine class to clean up CatManager, moved all generic state machine functionality and data out of CatManager and into the new class
       -Blocked out the structure of the new movement System; the methods and fields are put down, they need to be made functional.
    
    CatState.cs:
       -Deleted CatSubstate.cs; moved all substates into this script
       -Refactored CatState heavily; removed all broken functionality from existing states
       -Added State base class, CatState child class, and empty classes for each state that will be needed
    
    CatSubState.cs:
       -Deleted script

--- Git_Ragamuffin/Ragamuffin/Assets/Scripts/Cat/CatManager.cs
+++ Git_Ragamuffin/Ragamuffin/Assets/Scripts/Cat/CatManager.cs
@@ -9,20 +9,97 @@
 //	08/14/2019 Colby Peck: Added try/catch statement to ChangeCatState. 
 //	08/22/2019 Colby Peck:	 Made JumpLeft/Right and TurnLeft/Right methods. Cleaned up OnTriggerEnter.  Added State Dictionary with System.Type being the key. Added ChangeState overload for changing based on state type 
 // 08/22/2019 Colby Peck: Built AddState method that takes in a Type of CatState. Changed state machine to use AddState and the new ChangeState<>() methods. 
-// 08/22/2019 ColbyPeck: Changed ragTransform from a field to a property 
-
+// 08/22/2019 Colby Peck: Changed ragTransform from a field to a property 
+// 08/27/2019 Colby Peck: Removed commented-out code block containing old ChangeState methods 
+// 09/14/2019 Colby Peck: Added TODO list 
+// 09/15/2019 Colby Peck: Added StateMachine class to clean up CatManager, moved all generic state machine functionality and data out of CatManager and into the new class 
+// 09/15/2019 Colby Peck: Blocked out the structure of the new movement System; the methods and fields are put down, they need to be made functional. 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Diagnostics;
 
-public sealed class CatManager : MonoBehaviour
+//TODO:
+// [X] Move states from CatSubState to CatState, delete CatSubState.cs 
+//	[X] Implement Substates as States
+// [X] Refactor CatState 
+//		[X] Make sure we can access our parent (AVOID CIRCULAR INCLUDES)
+//		[X] Build methods for all the Monobehaviour methods we need (Update, FixedUpdate, etc.)
+//		[X] Make sure those methods are getting called in CatManager 
+// [X] Move Statemachine generic behaviour out of CatManager and into StateMachine 
+// [X] Block out movement system 
+//		[X] Write empty methods for the functionality we'll need 
+//		[X] Figure out what fields and properties will be necessary, add them. Serialize as needed 
+//		[X] Make sure states can access the required methods 
+// [ ] Build functionality of the Movement system 
+//		[ ] Build the cat's methods for perceiving the world 
+//		[ ] Build Patrol state and test movement system 
+// [ ] Make the other states 
+//		[ ] Alerted 
+//		[ ] Pusuit 
+//		[ ] Dazed 
+//		[ ] Flee
+//		[ ] Teleport 
+
+public class StateMachine : MonoBehaviour
 {
-	#region ToSort
-
-	[SerializeField] private float destinationDistance = .1f;
-
+	#region Fields and Properties
+	private Dictionary<System.Type, State> states = new Dictionary<System.Type, State>();
+	protected State currentState;
+	protected virtual StateMachine thisMachine { get { return this; } }
 	#endregion
+
+	#region State Management
+	protected void AddState<T>() where T : State, new() //T is a type that is required to be a CatState, and also have an empty constructor 
+	{
+		try
+		{
+			states.Add(typeof(T), new T()); //Try to add a new instance of the given state to the dictionary, use its type as the key 
+			states[typeof(T)].Init(thisMachine); //Because we made the constructor parameterless, any variables that need to be set on initialization must be set manually 
+		}
+		catch (System.Exception e) //Upon exception, 
+		{
+			UnityEngine.Debug.LogError("CatManager.AddState(): Exception generated! \n" + e.Message); //Tell the console what's happened 
+		}
+	}
+	public void ChangeState<T>()
+	{
+		try
+		{
+			currentState = states[typeof(T)];
+			currentState.Enable();
+		}
+		catch (System.Exception e)
+		{
+			if (e is KeyNotFoundException)
+			{
+				UnityEngine.Debug.LogError("Tried to change to invalid state type: " + typeof(T));
+			}
+			else
+			{
+				UnityEngine.Debug.LogError("CatManager.ChangeCatState(): Unforeseen exception generated: \n" + e.Message);
+			}
+		}
+	}
+	#endregion
+
+	#region Monobehaviour Methods
+	protected virtual void Update()
+	{
+		currentState.Tick();
+	}
+
+	protected virtual void FixedUpdate()
+	{
+		currentState.FixedTick();
+	}
+	#endregion
+
+}
+
+public class CatManager : StateMachine
+{
+
+	protected override StateMachine thisMachine { get { return this; } }
 
 	#region Serialized Variables
 	// Inspector assignable attributes
@@ -45,7 +122,6 @@ public sealed class CatManager : MonoBehaviour
 	[Header("Minimum distance cat can receive Rag damage")]
 	[SerializeField] private float miniumReceiveHitDistance;            // How close rag has to be to hit the cat. Delete this when hit functionality is implemented 
 	[Header("First speed is patrol. Second speed is chase")]
-	[SerializeField] private float[] moveSpeeds;                        /// Make this into two floats; baseSpeed and purSuitSpeed (?)
 	[SerializeField] private float timeBeforeFirstAttack;               // Self-explanatory 
 	[SerializeField] private float timeBetweenAttacks;                  // Self-explanatory 
 	[SerializeField] private float timeBetweenSearches;                 // Used to limit the number of raycasts per second 
@@ -66,8 +142,8 @@ public sealed class CatManager : MonoBehaviour
 	private byte randomChance;                                          // Used to store the cat's decision to jump 
 
 	// State Machine 
-	private Dictionary<System.Type, CatState> states = new Dictionary<System.Type, CatState>();
-	private CatState currentState;
+	//private Dictionary<System.Type, CatState> states = new Dictionary<System.Type, CatState>();
+	protected new CatState currentState { get { return (CatState)base.currentState; } set { base.currentState = value; } }
 
 
 	private float currentAttackTime;                                    // Time between attacks 
@@ -77,7 +153,6 @@ public sealed class CatManager : MonoBehaviour
 	private long lastAttack;                                            // Stores the time of the last attack, to be used for the calculation of when it should attack again 
 	private long lastRaycast;                                           // Stores the time of the last time it looked for Rag, to be used for the next time it looks 
 	private RaycastHit hitInfo;                                       // Object intersecting raycast 
-	private Stopwatch internalTimer;                                    /// Timing system needs to be reworked
 	private Transform raycastEye;                                       // Raycast start position (cat's eye) 
 	private Vector3 offset;                                             // Keeps the cat grounded for chase 
 	private Vector3 targetPosition;
@@ -191,19 +266,6 @@ public sealed class CatManager : MonoBehaviour
 	{
 		transform.rotation = Quaternion.Euler(0, 90, 0);
 	}
-
-	private void AddState<T>() where T : CatState, new() //T is a type that is required to inherit from CatState, and also have an empty constructor 
-	{
-		try
-		{
-			states.Add(typeof(T), new T()); //Try to add a new instance of the given state to the dictionary, use its type as the key 
-			states[typeof(T)].Init(this); //Because we made the constructor parameterless, any variables that need to be set on initialization must be set manually 
-		}
-		catch (System.Exception e) //Upon exception, 
-		{
-			UnityEngine.Debug.LogError("CatManager.AddState(): Exception generated! \n" + e.Message); //Tell the console what's happened 
-		}
-	}
 	#endregion
 
 	#region Initialization
@@ -217,22 +279,13 @@ public sealed class CatManager : MonoBehaviour
 		const float tDSetup = 25;
 		teleportDistance = (maxSearchDistance - tDSetup);
 
-		currentMoveSpeed = moveSpeeds[0];
-		//availableStates = new CatState[2] { new Unalerted(this), new Alerted(this) };
-
-
-		//currentState = availableStates[0];
-		AddState<Unalerted>();
-		AddState<Alerted>();
-		ChangeCatState<Unalerted>();
-
-		internalTimer = new Stopwatch();
-		internalTimer.Start();
-		//ragTransform = GameObject.FindGameObjectWithTag("Player").transform;        //FIX THIS LINE JESUS CHRIST 
+		AddState<Cat_Patrol>();
+		AddState<Cat_Alerted>();
+		AddState<Cat_Pursuit>();
+		ChangeState<Cat_Patrol>();
 		raycastEye = transform.GetChild(0);
 		offset = new Vector3(0, -1, 0);
 
-		AddState<Unalerted>();
 		// If start left is true in the inspector, the cat's will move left
 		//transform.rotation = (startLeftDirection) ? Quaternion.Euler(0, 270, 0) : Quaternion.Euler(0, 90, 0);
 
@@ -248,99 +301,21 @@ public sealed class CatManager : MonoBehaviour
 	}
 	#endregion
 
-	#region Main Update
-	private void Update()
-	{
-		// Updates the the cat state machine
-		currentState.UpdateState();
-	}
-	#endregion
-
 	#region Public Interface
-	public void AssignMoveSpeed(byte _index)
-	{
-		// Changes speed for walking, running, fleeing
-		currentMoveSpeed = moveSpeeds[_index];
-	}
 	public void CheckLocation()
 	{
-		// Still performs patrol like movement, but moves towards a specified point
+		// Still performs patrol like movement, but moves towards a specified point 
 		PatrolMovement();
 
-		// If cat has reached target, change back to unalerted
-		// State will change in patrol movement if Rag is found
+		// If cat has reached target, change back to unalerted 
+		// State will change in patrol movement if Rag is found 
 		if (AtDestination)
 		{
 			listeningForSound = false;
-			ChangeCatState<Unalerted>();
+			ChangeState<Cat_Patrol>();
 		}
 	}
 
-	///// <summary>
-	///// Changes the cat's current state
-	///// </summary>
-	///// <param name="_index">What is the index of the state we're changing to?</param>
-	//public void ChangeCatState(byte _index)
-	//{
-	//	// Self explanatory
-	//	try
-	//	{
-	//		currentState = availableStates[_index];
-	//		currentState.Enable();
-	//	}
-	//	catch (System.Exception e)
-	//	{
-	//		if (e is System.IndexOutOfRangeException)
-	//		{
-	//			UnityEngine.Debug.LogError("Tried to change to invalid state index: " + _index.ToString());
-	//		}
-	//		else
-	//		{
-	//			UnityEngine.Debug.LogError("CatManager.ChangeCatState(): Unforeseen exception generated: \n" + e.Message);
-	//		}
-	//	}
-	//}
-
-	///// <summary>
-	///// Changes the cat's current state
-	///// </summary>
-	///// <param name="type">What type is the state we're changing to?</param>
-	//public void ChangeCatState(CatStateType type)
-	//{
-	//	ChangeCatState((byte)type);
-	//}
-
-	public void ChangeCatState<T>()
-	{
-		try
-		{
-			currentState = states[typeof(T)];
-			currentState.Enable();
-		}
-		catch (System.Exception e)
-		{
-			if (e is KeyNotFoundException)
-			{
-				UnityEngine.Debug.LogError("Tried to change to invalid state type: " + typeof(T));
-			}
-			else
-			{
-				UnityEngine.Debug.LogError("CatManager.ChangeCatState(): Unforeseen exception generated: \n" + e.Message);
-			}
-		}
-	}
-
-	public void GetCurrentTime(bool _attack)
-	{
-		if (_attack)
-		{
-			lastAttack = internalTimer.ElapsedMilliseconds;
-		}
-		else
-		{
-			lastRaycast = internalTimer.ElapsedMilliseconds;
-		}
-	}
 	public void PatrolMovement()
 	{
 		// Debugging if check 
@@ -389,13 +364,7 @@ public sealed class CatManager : MonoBehaviour
 		// Debugging if check
 		if (canSearch)
 		{
-			//Used to restrict the amount of raycasts per second, because they can be expensive
-			if (internalTimer.ElapsedMilliseconds - lastRaycast > timeBetweenSearches * 1000)
-			{
-				LookForRag();
-				// Reassign last raycast
-				GetCurrentTime(false);
-			}
+
 		}
 	}
 	public void PlaySound(byte _index)
@@ -412,7 +381,7 @@ public sealed class CatManager : MonoBehaviour
 			if (DistToRag > sightDistance)
 			{
 				// Change back to unalerted patrol
-				ChangeCatState<Unalerted>();
+				ChangeState<Cat_Patrol>();
 			}
 		}
 
@@ -449,19 +418,13 @@ public sealed class CatManager : MonoBehaviour
 				transform.rotation = Quaternion.Euler(0, 270, 0);
 
 			// If cat is unalerted, toggle to alerted
-			if (currentState.GetType() == typeof(Unalerted))
+			if (currentState is Cat_Patrol)
 			{
-				ChangeCatState<Alerted>();
-
-				// Change to flee behavior
-				currentState.ChangeSubstate(1);
+				ChangeState<Cat_Alerted>();
 			}
 
 			fleeing = true;
 			attacking = false;
-
-			// Start fleeing
-			currentState.ChangeSubstate(1);
 		}
 	}
 	public void RunAway()
@@ -473,10 +436,7 @@ public sealed class CatManager : MonoBehaviour
 	{
 		listeningForSound = true;
 		targetPosition = _targetPosition;
-		ChangeCatState<Alerted>();
-
-		// Change to check behavior
-		currentState.ChangeSubstate(2);
+		ChangeState<Cat_Alerted>();
 	}
 	#endregion
 
@@ -486,26 +446,26 @@ public sealed class CatManager : MonoBehaviour
 		if (attacking)
 		{
 			//Used to time attacks
-			if (internalTimer.ElapsedMilliseconds - lastAttack > currentAttackTime * 1000)
-			{
-				// Put actual attack code here (damage player, play animation, etc.)
+			//if (internalTimer.ElapsedMilliseconds - lastAttack > currentAttackTime * 1000)
+			//{
+			//	// Put actual attack code here (damage player, play animation, etc.)
 
-				// Play attack audio
-				PlaySound(1);
+			//	// Play attack audio
+			//	PlaySound(1);
 
-				// Reassign last attack
-				GetCurrentTime(true);
+			//	// Reassign last attack
+			//	GetCurrentTime(true);
 
-				if (currentAttackTime != timeBetweenAttacks)
-					currentAttackTime = timeBetweenAttacks;
-			}
+			//	if (currentAttackTime != timeBetweenAttacks)
+			//		currentAttackTime = timeBetweenAttacks;
+			//}
 
-			return;
+			//return;
 		}
 
 		// Only makes it here before cat attacks
 		// Reset timer
-		GetCurrentTime(true);
+		//GetCurrentTime(true);
 
 		// Allow cat to attack on the next pass
 		attacking = true;
@@ -515,11 +475,11 @@ public sealed class CatManager : MonoBehaviour
 	}
 	private void LookForRag()
 	{
-		if (SeesRag) //If we see Rag, 
-		{
-			ChangeCatState<Alerted>();
-			currentState.ChangeSubstate(0); // Change to pursuit behavior 
-		}
+		//if (SeesRag) //If we see Rag, 
+		//{
+		//	ChangeCatState<Alerted>();
+		//	currentState.ChangeSubstate(0); // Change to pursuit behavior 
+		//}
 	}
 	private void Movement()
 	{
@@ -541,7 +501,7 @@ public sealed class CatManager : MonoBehaviour
 			{
 				if (canJump)    // Debugging if check
 				{
-					if (currentState.GetType() == typeof(Unalerted)) // If patrolling
+					if (currentState.GetType() == typeof(Cat_Patrol)) // If patrolling
 					{
 						randomChance = (byte)Random.Range(0, jumpDropChance);   // Retrieving random cat decision
 
@@ -606,11 +566,68 @@ public sealed class CatManager : MonoBehaviour
 			if (fleeing)    // If fleeing, change to unalerted
 			{
 				// Stop fleeing
-				currentState.ChangeSubstate(0);
-				ChangeCatState<Unalerted>();
+				ChangeState<Cat_Patrol>();
 				fleeing = false;
 			}
 		}
 	}
+	#endregion
+
+
+	#region New Movement System
+
+	//May be useful; if not, remove later 
+	public delegate void delegate_ReachedDesitnation();
+	public event delegate_ReachedDesitnation ReachedDestination;
+
+	//fields needed for determining behaviour
+	[SerializeField] private float destinationDistance = .5f;   //How close we need to be to our destination before we say that we've reached it
+
+	[SerializeField] private float baseMoveSpeed, pursuitMoveSpeed; //move speeds in units per second
+	private const float moveSpeedMultiplier = 1f / 50f; //This allows us to convert our units per second values into units per FixedUpdate tick without doing the division every tick. Division is kinda expensive. 
+
+	private Vector3 destination;
+	/// <summary>
+	/// Sets the cat's destination to a given Vector3
+	/// </summary>
+	/// <param name="destination">Where are we moving to?</param>
+	public void SetDestination(Vector3 destination)
+	{
+		// Tell our movement system to move towards the destination. 
+	}
+
+
+
+	private void Move()
+	{
+		//Move towards our destination
+		
+		//If we reach our destination, tell any listeners of our event that we reached it 
+		if (AtDestination)
+		{
+			if (ReachedDestination.GetInvocationList().Length > 0) //If we have any listeners, 
+			{
+				try
+				{
+					ReachedDestination.Invoke(); //Try to invoke our listeners 
+				}
+				catch (System.Exception e) //Upon exception, 
+				{
+					//Tell the console what happened 
+					if (e is System.NullReferenceException)
+					{
+						Debug.LogError("Catmanager.Movement(): Listener of reachedDestination found to be null!\n" + e.Message);
+					}
+					else
+					{
+						Debug.LogError("Catmanager.Movement(): unforeseen exception generated!\n" + e.Message);
+					}
+
+					return; //Arrest the code 
+				}
+			}
+		}
+	}
+
 	#endregion
 }
--- Git_Ragamuffin/Ragamuffin/Assets/Scripts/Cat/CatState.cs
+++ Git_Ragamuffin/Ragamuffin/Assets/Scripts/Cat/CatState.cs
@@ -5,109 +5,122 @@
 // Associated Scripts: 
 //--------------------------------------------------------------------------------------------------------------------------------------------------\\
 //Changelog
-//08/22/2019 Colby Peck: Added Init method, removed old constructors, added parameterless constructor 
+// 08/22/2019 Colby Peck: Added Init method, removed old constructors, added parameterless constructor 
+// 09/14/2019 Colby Peck: Deleted CatSubstate.cs; moved all substates into this script 
+// 09/14/2019 Colby Peck: Refactored CatState heavily; removed all broken functionality from existing states 
+// 09/14/2019 Colby Peck: Added State base class, CatState child class, and empty classes for each state that will be needed 
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class CatState
+public abstract class State
 {
-	#region Variables
-	protected System.Action<byte> AssignMoveSpeed;
-	public virtual void Init(CatManager _catManager) //Because we made the constructor parameterless, any variables that need to be set on initialization must be set manually using methods or properties 
+	#region Variables and Properties
+	protected StateMachine parent;
+	/// <summary>
+	/// The CatManager this state is attached to 
+	/// </summary>
+	//protected virtual StateMachine Parent { get { return parent; } set { parent = value; } }
+	protected bool ExitConditions() { return false; } //Might not end up using this; remove if proves to be unhelpful 
+
+	#endregion
+
+	#region Constructor
+	/// <summary>
+	/// Constructor <b>must</b> take no parameters
+	/// </summary>
+	public State() { }
+	#endregion
+
+	#region Methods
+	#region Initialization/De-Initialization
+	/// <summary>
+	/// This is called when this state is added to a CatManager machine
+	/// </summary>
+	/// <param name="_parent">What CatManager are we being attached to?</param>
+	//Because we made the constructor parameterless, any variables that need to be set on initialization must be set manually using methods or properties
+	public virtual void Init(StateMachine _parent)
 	{
-		AssignMoveSpeed = _catManager.AssignMoveSpeed;
-	}
-	#endregion
-
-	#region Initialization
-	public virtual void Enable()
-	{
-		return;
-	}
-	#endregion
-
-	#region Main Update
-	public abstract void UpdateState();
-	#endregion
-
-	#region Public Interface
-	public virtual void ChangeSubstate(byte _index)
-	{
-		return;
-	}
-
-	public CatState() { }
-	#endregion
-}
-public class Unalerted : CatState
-{
-	#region Variables
-	private System.Action PatrolMovement;
-	#endregion
-
-	#region Initialization
-	public Unalerted() { }
-
-	public override void Enable()
-	{
-		AssignMoveSpeed(0);
-	}
-	#endregion
-
-	#region Main Update
-	public override void UpdateState()
-	{
-		PatrolMovement();
-	}
-	#endregion
-}
-public class Alerted : CatState
-{
-	#region Variables
-	private System.Action<byte> PlaySound;
-	private AlertedStates[] availableStates;
-	private AlertedStates currentState;
-	#endregion
-
-	#region Initialization
-	public Alerted()
-	{
-
-	}
-	public override void Init(CatManager _catManager)
-	{
-		base.Init(_catManager);
-		PlaySound = _catManager.PlaySound;
-
-		availableStates = new AlertedStates[3]
+		parent = _parent;
+		if (parent == null)
 		{
-			new Pursuit(ref _catManager),
-			new Flee(ref _catManager),
-			new Check(ref _catManager)
-		};
-		currentState = availableStates[0];
+			Debug.LogError(GetType().ToString() + ": Parent found to be null");
+		}
 	}
-	public override void Enable()
-	{
-		AssignMoveSpeed(1);
-		PlaySound(0);
-	}
+
+
+
+	/// <summary>
+	/// This method is called whenever this state is changed to
+	/// </summary>
+	public virtual void Enable() { }
+	/// <summary>
+	/// This method is called whenever the CatManager changes to a different state
+	/// </summary>
+	public virtual void Disable() { }
 	#endregion
 
-	#region Main Update
-	public override void UpdateState()
-	{
-		currentState.UpdateState();
-	}
-	#endregion
+	#region Updates
+	/// <summary>
+	/// Called in Update
+	/// </summary>
+	public virtual void Tick() { }
 
-	#region Public Interface
-	public override void ChangeSubstate(byte _index)
-	{
-		currentState = availableStates[_index];
-		currentState.Enable();
-	}
+	/// <summary>
+	/// Called in FixedUpdate
+	/// </summary>
+	public virtual void FixedTick() { }
 	#endregion
+	#endregion
+}
+
+public abstract class CatState : State
+{
+	protected new CatManager Parent { get { return (CatManager)base.parent; } set { parent = value; } } //Declare our parent as a CatManager so we can access all its specific stuff 
+
+	//Need to refactor Init to set our parent correctly 
+	public void Init(CatManager _parent)
+	{
+		Parent = _parent;
+
+		//Bit of error checking is always good 
+		if (!(Parent is CatManager)) //If our parent isn't a CatManager, 
+		{
+			Debug.LogError(GetType().ToString() + ": Parent not CatManager!"); //Yell at the console about it 
+		}
+	}
+}
+public class Cat_Patrol : CatState
+{
+	//Patrols between set points, changes to pursuit state if he sees rag.Needs functionality for returning to patrol from other states 
+	public override void Tick()
+	{
+		Parent.SetDestination(new Vector3(1,1,1));
+	}
+}
+
+public class Cat_Alerted : CatState
+{
+	//Player makes a noise, causing the cat to come investigate.  
+}
+
+public class Cat_Pursuit : CatState
+{
+	//Cat sees player, is currently chasing them. Upon getting close enough, the cat will begin attacking the player. If the player successfully counter-attacks, the cat will become dazed. 
+}
+
+public class Cat_Dazed : CatState
+{
+	// Remains dazed for a given amount of time, becomes undazed if he goes off-screen. Reverts to patrol state when exiting state. 
+}
+
+public class Cat_Flee : CatState
+{
+	//Cat is hurt from some source: runs away to a given location, stays there for a given amount of time, then reverts to patrol state. 
+}
+
+public class Cat_Teleport
+{
+	//Cat is meant to move to a point in front of the player, but he’s behind the player. Play an animation of the cat running across the foreground, teleport the cat machine then set the cat to the patrol state at its new location.
 }
