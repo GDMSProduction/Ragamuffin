1fc8478eb0cb24fd451e99b7633006e001de26d3
DigitalBeef <54082789+DigitalBeef@users.noreply.github.com>
2019-09-29 20:06:13 -0400

    Revert "Merge branch 'master' of https://github.com/GDMSProduction/Ragamuffin"
    This reverts commit fe64e6a051c15ec41d248040ffaeae93625fada9, reversing
    changes made to 7c1383f9586e74f3fb91c2a3824188b647ff080a.

--- Git_Ragamuffin/Ragamuffin/Assets/Scripts/UI/MainMenu.cs
+++ Git_Ragamuffin/Ragamuffin/Assets/Scripts/UI/MainMenu.cs
@@ -1,83 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
-{
-    [Header("=============================================================================================================================")]
-    [Header("Warning window")]
-    [SerializeField] GameObject warningWindow;
-    [Tooltip("The Warning Window Title Text Object")]
-    [SerializeField] TMP_Text text;
-    [SerializeField] Button yesButton, noButton;
-    [Header("=============================================================================================================================")]
+public class MainMenu : MonoBehaviour {
 
     public int savedSceneIndex;
 
+	public void StartGame()
+    {
+        //This will load the scene directly after the current scene. 
+        //To add a scene to the scene manager or change the current order
+        //go to FILE -> BUILD SETTINGS and modify the scenes how you see fit. 
+        //The main menu should always be index 0.
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
     public void OptionsMenu()
     {
-        LoadScene("OptionsMenu");
+        PlayerPrefs.SetString("lastScene", SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("OptionsMenu");
     }
-
     public void ContinueGame()
     {
         //Continues the game when passed a specific scene based on a passed index.
-        LoadScene(savedSceneIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + savedSceneIndex);
     }
-
-    public void QuitGame(string quitMessageTitle)
+    public void QuitGame()
     {
-        //Make sure player meant to do this first here
-        EnableWarningWindow(quitMessageTitle, ExitApplication);
-    }
-
-    /// <summary>
-    /// WARNING: THIS FUNCTION IMMEDIATELY EXITS THE APPLICATION!!
-    /// </summary>
-    public void ExitApplication()
-    {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
-
-        //Ends the program.
+        //Ends the program. 
         Application.Quit();
     }
 
-
-    //TEMPORARY - WILL BE CHANGED TO ASYNC LATER
-    public void LoadScene(string levelName)
-    {
-        SceneManager.LoadScene(levelName);
-    }
-
-    public void LoadScene(int levelIndex)
-    {
-        SceneManager.LoadScene(levelIndex);
-    }
-
-    public void EnableWarningWindow(string windowTitle, UnityEngine.Events.UnityAction yesClicked, UnityEngine.Events.UnityAction noClicked = null, string yesButtonOverride = "Yes", string noButtonOverride = "No")
-    {
-        yesButton.onClick.RemoveAllListeners();
-        noButton.onClick.RemoveAllListeners();
-        text.text = windowTitle;
-        yesButton.onClick.AddListener(yesClicked);
-        yesButton.onClick.AddListener(WarningWindowClose);
-        yesButton.GetComponentInChildren<TMP_Text>().text = yesButtonOverride;
-        if (noClicked != null)
-            noButton.onClick.AddListener(noClicked);
-        noButton.onClick.AddListener(WarningWindowClose);
-        noButton.GetComponentInChildren<TMP_Text>().text = noButtonOverride;
-        warningWindow.SetActive(true);
-    }
-
-    public void WarningWindowClose()
-    {
-        warningWindow.SetActive(false);
-    }
-
-    
 }
