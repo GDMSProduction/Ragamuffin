7ff8d45142296ef08a60dc2947cf2465d1300644
Colby Peck <CLPeck1@student.fullsail.edu>
2020-03-02 03:31:00 -0500

    RenderDisabler
    -Added script for turning off renderers at start of runtime

---
+++ Git_Ragamuffin/Ragamuffin/Assets/Scripts/HelperScripts/RenderDisabler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class disables the renderers of the object it's attached to and 
+/// </summary>
+public class RenderDisabler : MonoBehaviour
+{
+	[SerializeField] private bool scriptEnabled = true;
+
+	[Tooltip("Disable renderers of child objects as well?")]
+	[SerializeField] private bool includeChildren = false;
+
+	private Renderer[] rends;
+
+	void Start()
+	{
+		//this is here so the renderers will ALWAYS be turned off in builds, even if someone forgets to enable the render disabler before building 
+#if UNITY_EDITOR
+		if(!scriptEnabled)
+		{
+			enabled = false;
+			return;
+		}
+#endif
+
+		if (includeChildren)
+		{
+			rends = GetComponentsInChildren<Renderer>();
+		}
+		else
+		{
+			rends = GetComponents<Renderer>();
+		}
+
+		for (int i = 0; i < rends.Length; i++)
+		{
+			rends[i].enabled = false;
+		}
+	}
+}
