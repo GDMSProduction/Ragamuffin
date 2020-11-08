67187fd24199eec3f9f642a4bf2f8956a408f6bf
kodo21 <kodo21hood@gmail.com>
2020-11-08 02:02:35 -0500

    Set starter content for picture collectables.

---
+++ Git_Ragamuffin/Ragamuffin/Assets/Scripts_SMC/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject menu;
+    private bool onOff = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            onOff = !onOff;
+            menu.SetActive(onOff);
+        }
+    }
+}
