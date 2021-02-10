37ee5dfcc943659fc5a9fb9b1e6042ffdf079d4b
Skievvv <kodo21hood@gmail.com>
2021-02-09 21:18:05 -0500

    Update Folders
    Updating folders and moving things around to organize better.

---
+++ Git_Ragamuffin/Ragamuffin/Assets/0Scripts/CurrentScripts/OnOff2.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class OnOff2 : MonoBehaviour
+{
+    public GameObject goOnOff;
+    public GameObject otherOnOff;
+    public void ObjectOnOff()
+    {
+        goOnOff.SetActive(false);
+        otherOnOff.SetActive(true);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        ObjectOnOff();
+    }
+}
