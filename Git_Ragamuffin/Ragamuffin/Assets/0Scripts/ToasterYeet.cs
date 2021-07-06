b1e7a3f0e5b125d9c0f9588239f464cac724c337
Mattsbiome <matthewxcharles97@gmail.com>
2021-07-06 04:16:47 -0400

    Adding Force to toaster activation
    Toaster now applies proper force to the player to assist them into getting into the upper platforms

--- Git_Ragamuffin/Ragamuffin/Assets/0Scripts/ToasterYeet.cs
+++ Git_Ragamuffin/Ragamuffin/Assets/0Scripts/ToasterYeet.cs
@@ -5,43 +5,40 @@ using UnityEngine;
 public class ToasterYeet : MonoBehaviour
 {
     public Animator toasterAnim;
+    public Rigidbody rb;
+    public float force;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        //toasterAnim = gameObject.GetComponentInParent<Animator>();
         toasterAnim = this.gameObject.GetComponent<Animator>();
     }
-
-    // Update is called once per frame
     void Update()
     {
         
     }
-
     private void OnTriggerEnter(Collider other)
     {
         toasterAnim.SetBool("Yeeting", true);
 
+        rb = other.GetComponent<Rigidbody>();
+
         Invoke("Yeet", 0.5f);
-
-        Debug.Log("yeet");
+        Invoke("Base", 1.0f);
     }
-
     void OnTriggerExit()
     {
         toasterAnim.SetBool("Yeeting", false);
         toasterAnim.SetBool("Reset", true);
     }
-
     public void Base()
     {
         toasterAnim.SetBool("Reset", false);
     }
     public void Yeet()
     {
+        rb.AddForce(1, force, 0, ForceMode.Impulse);
 
+        Debug.Log("yeet");
     }
  
 }
