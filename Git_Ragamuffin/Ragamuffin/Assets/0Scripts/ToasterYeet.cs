a48a92388c788a2a0564cb1e2116c930a2345beb
Mattsbiome <matthewxcharles97@gmail.com>
2021-07-05 23:39:07 -0400

    Working on Slice
    Adding slice scene and toaster

---
+++ Git_Ragamuffin/Ragamuffin/Assets/0Scripts/ToasterYeet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToasterYeet : MonoBehaviour
+{
+    public Animator toasterAnim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //toasterAnim = gameObject.GetComponentInParent<Animator>();
+        toasterAnim = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        toasterAnim.SetBool("Yeeting", true);
+
+        Invoke("Yeet", 0.5f);
+
+        Debug.Log("yeet");
+    }
+
+    void OnTriggerExit()
+    {
+        toasterAnim.SetBool("Yeeting", false);
+        toasterAnim.SetBool("Reset", true);
+    }
+
+    public void Base()
+    {
+        toasterAnim.SetBool("Reset", false);
+    }
+    public void Yeet()
+    {
+
+    }
+ 
+}
