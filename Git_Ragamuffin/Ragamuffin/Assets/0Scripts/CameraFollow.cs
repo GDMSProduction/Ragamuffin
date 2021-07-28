b503bc9c41bbdaed8baf0ba647f91e2694ecdc17
Mattsbiome <matthewxcharles97@gmail.com>
2021-07-28 17:20:48 -0400

    Adding Camera Alter position
    Can toggle camera position between different offsets

--- Git_Ragamuffin/Ragamuffin/Assets/0Scripts/CameraFollow.cs
+++ Git_Ragamuffin/Ragamuffin/Assets/0Scripts/CameraFollow.cs
@@ -4,12 +4,51 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;
     public float smoothSpeed;
-    public Vector3 offset;
+    public Vector3 offsetBase; // base offset of camera
+    public Vector3 offsetFOV; // offset for larger view area
+    public bool baseView = true;
+    public bool FOV = false;
+
 
     private void FixedUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
+        if (baseView && !FOV) { Base(); };
+        if (!baseView && FOV) { fov(); }
+    }
+
+
+    public void Base()
+    {
+        Vector3 desiredPosition = target.position + offsetBase;
         Vector3 smoothedPositon = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPositon; // Allows camera to follow the player.
     }
+
+    public void fov()
+    {
+        Vector3 desiredPosition = target.position + offsetFOV;
+        Vector3 smoothedPositon = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPositon; // Allows camera to follow the player.
+    }
+
+    public void ToggleView()
+    {
+        if(baseView == true)
+        {
+            baseView = false;
+        }
+        else
+        {
+            baseView = true;
+        }
+        if (FOV == false)
+        {
+            FOV = true;
+        }
+        else
+        {
+            FOV = false;
+        }
+    }
+
 }
