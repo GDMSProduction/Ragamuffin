0e19d8f6bf22dfe3aaee187b5e9d39e22d5eb997
Mattsbiome <matthewxcharles97@gmail.com>
2021-09-22 22:05:26 -0400

    adding next scene loading

---
+++ Git_Ragamuffin/Ragamuffin/Assets/0Scripts/LoadNextLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadNextLevel : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    { 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
